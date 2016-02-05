ec040ea64cbb0782a369213703042bac5ddfbe5d
Karl Burkhardt <karlaburkhardt@gmail.com>
2016-02-04 18:36:34 -0600

    Update scrape.py
    Added a check for pre-existing files and allowed for more than one ticker to be downloaded at a time.

--- scrape.py
+++ scrape.py
@@ -1,20 +1,22 @@
-"""
-Yahoo Finance saves all their .csv files, which house the historical
-prices of a given ticker, under the same url with the exception of
-the ticker. This function will scrape this data for all tickers given
-as inputs.
-"""
+import urllib.request
+import os.path
 
-def scrape():
+def scrape(tickers):
     
-    import urllib.request
+    url1 = 'http://real-chart.finance.yahoo.com/table.csv?s='
+    url2 = '&d=0&e=22&f=2016&g=d&a=0&b=2&c=1962&ignore=.csv'
     
-    url = 'http://real-chart.finance.yahoo.com/table.csv?s=KO&d=0&e=22&f=2016&g=d&a=0&b=2&c=1962&ignore=.csv'
+    for tick in tickers:
+        if os.path.isfile(tick + '.csv') == False: # checks for existing file
+            url = url1 + tick + url2
+            file_name = tick + '.csv'
+            urllib.request.urlretrieve(url, file_name)
+            print('downloaded!')
+#        else: # downloads new file as ticker.csv
+#            break
 
-    urllib.request.urlretrieve(url, 'KO.csv')
-
-    f = open('KO.csv','r')
+    f = open(tick + '.csv','r')
     read_file = f.read()
     file_rows = read_file.split('\n')
     
-    # print(file_rows)
+    print(file_rows[1])
